3751f566dfba9f67d3f1ff3b5eb67e1199a0ed3e
vesnna <vesna.nikita@yandex.ru>
2021-03-09 23:01:28 +0300

    add one more enemy texture, refactor movement logic

--- WarFly/Enemy.swift
+++ WarFly/Enemy.swift
@@ -9,8 +9,10 @@ import SpriteKit
 
 class Enemy: SKSpriteNode {
     static var textureAtlas: SKTextureAtlas?
+    var enemyTexture: SKTexture!
     
-    init() {
+    init(enemyTexture: SKTexture) {
+        let texture = enemyTexture
         let enemyTexture = Enemy.textureAtlas?.textureNamed("airplane_4ver2_13")
         super.init(texture: enemyTexture, color: .clear, size: CGSize(width: 221, height: 204))
         self.xScale = 0.5
@@ -22,12 +24,15 @@ class Enemy: SKSpriteNode {
     func flyEnemy() {
         let screenSize = UIScreen.main.bounds
         let timeHorizontal = 3.0
-        let timeVertical = 10.0
+        let timeVertical = 6.0
+        
         let moveLeft = SKAction.moveTo(x: 50, duration: timeHorizontal)
         moveLeft.timingMode = .easeInEaseOut
         let moveRight = SKAction.moveTo(x: screenSize.width - 50, duration: timeHorizontal)
         moveRight.timingMode = .easeInEaseOut
-        let enemyFlySequence = SKAction.sequence([moveLeft,moveRight])
+        
+        let randomNumber = Int(arc4random_uniform(2))
+        let enemyFlySequence = randomNumber == EnemyDirection.left.rawValue ? SKAction.sequence([moveLeft, moveRight]) : SKAction.sequence([moveRight, moveLeft])
         let infiniteFlyMovement = SKAction.repeatForever(enemyFlySequence)
         
         let downMovement = SKAction.moveTo(y: -100, duration: timeVertical)
@@ -40,4 +45,9 @@ class Enemy: SKSpriteNode {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+enum EnemyDirection: Int {
+    case left = 0
+    case right
 }
